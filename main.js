25ebd7bf141830319e6570508ba05fe56c8b64b8
Nabil Fannane <mzabi30@gmail.com>
2018-12-28 10:39:29 -0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-$(document).ready(()=>{
-console.log('asdljadsfkl')
-})
+
+
+$(document).ready(function () {
+
+    $('#header').ripples({
+        dropRadius: 4,
+        perturbance: .1,
+    
+      });
+
+
+
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+        // other options
+      });
+  });

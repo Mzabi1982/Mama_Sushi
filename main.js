9a862726427f53c68410ec382cf4f96039ea3117
Nabil Fannane <mzabi30@gmail.com>
2018-12-28 16:07:00 -0800

    test

--- main.js
+++ main.js
@@ -18,4 +18,29 @@ $(document).ready(function () {
         }
         // other options
       });
-  });
+
+
+       //   const toggler=document.querySelector('.toggler-btn');
+    //   const bar1=document.querySelector('.bar1');
+    //   const bar2=document.querySelector('.bar2');
+    //   const bar3=document.querySelector('.bar3');
+ 
+    $('.toggler-btn').click(()=>{
+        $('.toggler-btn').toggleClass('change')
+    })
+
+
+    $(window).scroll(()=>{
+        let position = $(this).scrollTop();
+        if(position>=718){
+            $('#nav').addClass('background-padding');
+            $('#nav').addClass('fixed-top');
+        }else{
+            $('#nav').removeClass('');
+            $('#nav').removeClass('fixed-top');
+        }
+    })
+
+
+
+});

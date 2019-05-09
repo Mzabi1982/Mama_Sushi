0064cc0b6ae848ef6b1655112d530c6e094eb0da
Nabil Fannane <nabil@goadroit.com>
2019-05-09 11:34:41 -0700

    last commit

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
-
-
 $(document).ready(function () {
+
+    document.querySelector('#bte').addEventListener('click', e => {
+        e.preventDefault()
+    })
 
     $('#header').ripples({
         dropRadius: 4,
         perturbance: .1,
-    
-      });
+
+    });
 
 
 
     $('.parent-container').magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
         type: 'image',
-        gallery:{
-            enabled:true
+        gallery: {
+            enabled: true
         }
         // other options
-      });
+    });
 
 
-       //   const toggler=document.querySelector('.toggler-btn');
+    //   const toggler=document.querySelector('.toggler-btn');
     //   const bar1=document.querySelector('.bar1');
     //   const bar2=document.querySelector('.bar2');
     //   const bar3=document.querySelector('.bar3');
- 
-    $('.toggler-btn').click(()=>{
+
+    $('.toggler-btn').click(() => {
         $('.toggler-btn').toggleClass('change')
     })
 
 
-    $(window).scroll(()=>{
+    $(window).scroll(() => {
         let position = $(this).scrollTop();
-        if(position>=718){
+        if (position >= 718) {
             $('#nav').addClass('background-padding');
             $('#nav').addClass('fixed-top');
-        }else{
+        } else {
             $('#nav').removeClass('');
             $('#nav').removeClass('fixed-top');
         }
